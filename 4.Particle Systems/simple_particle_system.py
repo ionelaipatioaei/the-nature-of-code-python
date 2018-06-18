10ae4d8ed1636457f55ae2e8f6a3dde713d85af0
Ionel Aipatioaei <lionel.developer72@gmail.com>
2018-06-19 00:31:31 +0300

    comments...

--- 4.Particle Systems/simple_particle_system.py
+++ 4.Particle Systems/simple_particle_system.py
@@ -2,11 +2,14 @@ from p5 import *
 from random import uniform
 
 class Particle:
+    """The particle class defines the behaviour of a single particle"""
+
     def __init__(self, pos, size):
         self.pos = pos
         self.vel = Vector(0, 0)
         self.acc = Vector(uniform(-0.009, 0.009), uniform(0, 0.1))
         self.size = size
+        # The lifespan is used to to define how much the particle will live
         self.lifespan = 255
 
     def show(self):
@@ -24,6 +27,8 @@ class Particle:
         return self.lifespan <= 0
 
 class ParticleSystem:
+    """The particle system class is used to group together multiple particles"""
+
     def __init__(self, pos):
         self.pos = pos
         self.elements = []
@@ -31,9 +36,11 @@ class ParticleSystem:
     def update(self):
         x_coord = uniform(self.pos.x - 25, self.pos.x + 25)
         y_coord = uniform(self.pos.y - 25, self.pos.y + 25)
+        # Every 7 frames a particle is born
         if frame_count % 7 == 0:
             self.elements.append(Particle(Vector(x_coord, y_coord), uniform(10, 20)))
 
+        # It is important to loop through an array backwards
         for i in range(len(self.elements) - 1, -1, -1):
             self.elements[i].show()
             self.elements[i].update()
@@ -51,14 +58,16 @@ def setup():
 def draw():
     global particle_systems
     background(255)
-
+    
     if len(particle_systems) > 0:
         for particle_system in particle_systems:
             particle_system.update()
 
-    print(len(particle_systems))
+    # print(len(particle_systems))
 
+# This function is invoked only once when the mouse is pressed even if keep it pressed 
 def mouse_pressed():
+    # Creates a new particle system at the mouse coords
     particle_systems.append(ParticleSystem(Vector(mouse_x, mouse_y)))
 
 run()
